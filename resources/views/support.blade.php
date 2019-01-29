59735720c48eb5ec5c428970ed57dc6fe13b1ad2
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-01-30 01:13:05 +0530

    Added FAQ Content

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -4,36 +4,114 @@
   <div class="row">
     <div class="col-md-12">
       <div id="card-645473">
+
         <div class="card">
           <div class="card-header">
             <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-1">I just signed up, what are my next steps?</a>
           </div>
           <div id="card-element-1" class="collapse">
             <div class="card-body">
-              Welcome to Meal Basket, we’re excited to start cooking with you! Now that you’re signed up, we recommend reviewing your Account
-              Details and exploring the menus on our Home page.
+             Welcome to Meal Basket, we’re excited to start cooking with you! Now that you’re signed up, we recommend reviewing your Account Details and exploring the menus on our Home page.
             </div>
           </div>
         </div>
 
         <div class="card">
           <div class="card-header">
-            <a class="collapsed card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-2">What types of payment can I use?</a>
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-2">Forgot my Login information, What do I do next?</a>
           </div>
           <div id="card-element-2" class="collapse">
             <div class="card-body">
-              We currently accept Visa, MasterCard, American Express, and Discover. We also offer a payment via cash.
+            Click on Forgot My Password, and proceed with the steps.
             </div>
           </div>
         </div>
 
         <div class="card">
           <div class="card-header">
-            <a class="collapsed card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-3">Where do you deliver?</a>
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-3">How do I cook this recipe?</a>
           </div>
           <div id="card-element-3" class="collapse">
             <div class="card-body">
-              We deliver across the state of Goa.
+             Meal Basket’s staff has written and compiled hundreds of helpful articles to inspire you and help you become a confident and successful home cook.
+             When you’re on a recipe page, you will see the list of Ingredients and the Directions to cook the dish. Just follow the procedure.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-4">Where do you deliver?</a>
+          </div>
+          <div id="card-element-4" class="collapse">
+            <div class="card-body">
+            We deliver across the state of Goa
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-5">What types of payment can I use?</a>
+          </div>
+          <div id="card-element-5" class="collapse">
+            <div class="card-body">
+            We currently accept Visa, MasterCard, American Express, and Discover. We also offer a payment via cash.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-6">How do I find a recipe?</a>
+          </div>
+          <div id="card-element-6" class="collapse">
+            <div class="card-body">
+            You can use the Search Bar at the top left corner to directly find the recipe.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-7">How do I order just the ingredient?</a>
+          </div>
+          <div id="card-element-7" class="collapse">
+            <div class="card-body">
+            You can use the "Ingredient Search" option at the top left corner and place the order as per your need.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-8">How do I contact customer service?</a>
+          </div>
+          <div id="card-element-8" class="collapse">
+            <div class="card-body">
+            You can contact us at Email: support@mealbasket.in, or Ph: (+91)1234567890
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-9">Do I need to register to look up for a recipe?</a>
+          </div>
+          <div id="card-element-9" class="collapse">
+            <div class="card-body">
+            No, you need to register only when you want to place an order.
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <a class="card-link" data-toggle="collapse" data-parent="#card-645473" href="#card-element-10">How do i select the quantity of ingredients?</a>
+          </div>
+          <div id="card-element-10" class="collapse">
+            <div class="card-body">
+            When you select a recipe, you need to provide the number of servings. The ingredients will be scaled accordinlgy to the number of servings.
             </div>
           </div>
         </div>

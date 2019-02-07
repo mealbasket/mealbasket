fcff326adc941c940779f609a9d8e05886ffd2e1
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2019-02-07 11:54:27 +0530

    Created ingredient migration,model,controller

---
+++ database/migrations/2019_02_07_050530_create_ingredients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->unsignedInteger('unit_id');
+            $table->foreign('unit_id')->references('id')->on('ing_units');
+            $table->integer('price');
+            $table->integer('base_quantity');
+            $table->string('img_path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}

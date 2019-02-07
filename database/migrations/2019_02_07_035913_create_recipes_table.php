8642d58dd46d3ab12b4317140e8aa39b6137b9fa
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2019-02-07 10:17:18 +0530

    Created recipe migration,model,controller

---
+++ database/migrations/2019_02_07_035913_create_recipes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('prep_time');
+            $table->integer('servings');
+            $table->integer('difficulty');
+            $table->decimal('rating', 4, 2);
+            $table->boolean('approved');
+            $table->string('site_id');
+            $table->integer('category_id');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}

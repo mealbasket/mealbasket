a3eaefdb066cfe3572ec1891cea73eaf4c34e385
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-02-19 22:02:38 +0530

    Fix CSS of recipe and ingredient index

--- resources/views/recipe/index.blade.php
+++ resources/views/recipe/index.blade.php
@@ -5,12 +5,17 @@
   <div class="row">
     @foreach($recipes as $recipe)
     <div class="col-md-2 py-2">
-      <div class="card">
+      <div class="card" style="width: fit-content;">
         <div class="card-block text-center py-2">
           @foreach ($recipe->Images as $image)
-          <img src="{{$image}}" alt="{{$recipe->name}}" style="height: 150px; width: auto;"> @endforeach
+          <img src="{{$image}}" alt="{{$recipe->name}}" style="height: 150px; width: auto;">
+          @endforeach
         </div>
-        <p class="card-title font-weight-bold">{{$recipe->name}}</p>
+        <p class="card-title font-weight-bold text-center">{{$recipe->name}}</p>
+        <p class="card-title font-weight-bold text-center">~ ₹{{$recipe->price}}/-</p>
+        <div class="row justify-content-center">
+          <button type="button" class="btn btn-outline-success" style="width: 50%">View Details</button>
+        </div>
       </div>
     </div>
     @endforeach {{$recipes->links("pagination::bootstrap-4")}}

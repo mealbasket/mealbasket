defc8acb393004faa554462453d74a68b4559c06
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-03-04 19:39:58 +0530

    Fix search buttons

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,27 +1,25 @@
 @extends('layouts.app') 
 @section('content')
 <div class="row pt-4 justify-content-center">
-    <div class="col-md-12">
+  <div class="col-md-6">
     <h3>Search</h3>
     <form action="/search" method="POST">
       @csrf
       <div class="row px-3">
         <div class="input-group mb-3">
           <input type="text" class="form-control" name="query" placeholder="Enter search query">
-          <div class="input-group-append">
-            <div class="dropdown text-right">
-              <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Search
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <button name="type" value="recipe" class="dropdown-item">Recipe Search</button>
-                <button name="type" value="ingredient" class="dropdown-item">Ingredient Search</button>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-    </form>
-    </div>
+    
+  </div>
+  <div class="col-md-4">
+    <div class="row justify-content-center" style="padding-top: 40px;">
+  <span>
+      <button name="type" value="recipe" class="btn btn-primary">Recipe Search</button>
+      <button name="type" value="ingredient" class="btn btn-info">Ingredient Search</button>
+    </span>
+  </div>
+  </div>
+</form>
 </div>
 @endsection

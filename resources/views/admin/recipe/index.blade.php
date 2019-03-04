6f18eebc8f031fc505adc93d1221040859edf18b
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-03-05 01:00:44 +0530

    Fix manage recipe CSS and added View button

--- resources/views/admin/recipe/index.blade.php
+++ resources/views/admin/recipe/index.blade.php
@@ -4,21 +4,22 @@
   @if(count($recipes)>0)
   <div class="row">
     @foreach($recipes as $recipe)
-    <div class="col-md-2 py-2">
+    <div class="col-6 col-md-2 py-2">
       <div class="card ingOrRecipeCard">
-        <p class="card-title font-weight-bold text-center">{{$recipe->name}}</p>
+        <p class="card-title font-weight-bold text-center mt-3">{{$recipe->name}}</p>
         @if($recipe->isApproved())
-        <a class="btn btn-warning" href="#" role="button" onclick="document.getElementById('approval-form').submit();">Disapprove</a>
+        <a class="btnAdmin btn btn-warning" href="#" role="button" onclick="document.getElementById('approval-form').submit();">Disapprove</a>
         @else
-        <a class="btn btn-success" href="#" role="button" onclick="document.getElementById('approval-form').submit();">Approve</a>
+        <a class="btnAdmin btn btn-success" href="#" role="button" onclick="document.getElementById('approval-form').submit();">Approve</a>
         @endif
         <form id="approval-form" action="{{ url('recipes/'.$recipe->id) }}" method="POST" style="display: none;">
           @method('PUT')
           @csrf
           <input type="hidden" name="approved" value="{{$recipe->isApproved() ? '0' : '1'}}">
         </form>
-        <a class="btn btn-info" href="#" role="button">Edit</a>
-        <a class="btn btn-danger" href="#" role="button">Delete</a>
+        <a class="btnAdmin btn btn-success" target="_blank" href="{{ url( 'recipes/' . $recipe->id ) }}" role="button">View</a>
+        <a class="btnAdmin btn btn-info" href="#" role="button">Edit</a>
+        <a class="btnAdmin btn btn-danger" href="#" role="button">Delete</a>
       </div>
     </div>
     @endforeach {{$recipes->links("pagination::bootstrap-4")}}

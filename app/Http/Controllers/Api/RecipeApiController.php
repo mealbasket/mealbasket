9e13d8e3a29810e1aa3874a99809d2d35b0ed8c8
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2019-03-06 11:46:20 +0530

    Recipe Api

---
+++ app/Http/Controllers/Api/RecipeApiController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Recipe;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RecipeApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $recipe = new Recipe();
+        $data = $request->only($recipe->getFillable());
+        $recipe->fill($data);
+        $recipe->image_path = $request->photo->store('recipe_images', 's3');
+        $recipe->save();
+        return response()->json(['success' => 'true', 'id' => $recipe->id], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Recipe  $recipe
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Recipe $recipe)
+    {
+        return $recipe;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Recipe  $recipe
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Recipe $recipe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Recipe  $recipe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Recipe $recipe)
+    {
+        //
+    }
+
+    public function checkBySiteId($site_id)
+    {
+        $r = Recipe::where('site_id', $site_id)->first();
+        if ($r)
+        {
+            return response()->json(['exists' => 'true', 'id' => $r->id], 200);
+        }
+        else
+        {
+            return response()->json(['exists' => 'false'], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Api')->group(function () { 
+    Route::apiResource('recipe', 'RecipeApiController');
+    Route::get('recipe/check/{site_id}', 'RecipeApiController@checkBySiteId');
+
 });

9dd3ec3bc46cf0d7ce6b3c43ee769191907863f4
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-04-03 20:12:05 +0530

    Fix order page css

--- resources/views/user/orders.blade.php
+++ resources/views/user/orders.blade.php
@@ -1,10 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 @section('pagespecificstyles')
 <link href="{{ asset('/css/stepbar.css') }}" rel="stylesheet">
 <script src="{{ asset('/js/stepbar.js') }}"></script>
 @endsection
+ 
 @section('content')
 <h3>My Orders</h3>
+@if($orders=="[]")
+<p>No orders</p>
+@else
 <div class="pt-4 row justify-content-center">
     <div class="col-md-12">
         @foreach($orders as $order)
@@ -14,7 +18,7 @@
                 <div id="steps-head-{{$order->id}}" class="py-2" style="position: sticky;"></div>
 
                 <script>
-                        $('#steps-head-{{$order->id}}').append("<div id='steps-{{$order->id}}'></div>");
+                    $('#steps-head-{{$order->id}}').append("<div id='steps-{{$order->id}}'></div>");
                         $('#steps-{{$order->id}}').progressbar({
                       // ~: failed
                       // @: current
@@ -31,70 +35,70 @@
             </div>
             <div class="card-body">
                 <div style="overflow-x: auto;">
-                <table id="cart" class="table table-hover table-condensed">
-                    <thead>
-                        <tr>
-                            <th style="width:50%">Product</th>
-                            <th style="width:10%">Price</th>
-                            <th style="width:8%">Quantity</th>
-                            <th style="width:15%" class="text-center">Subtotal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {{--
-                        <h3>Ingredients</h3> --}} @foreach($order->Ingredients as $i)
-                        <tr>
-                            <td data-th="Product">
-                                <div class="row">
-                                    <div class="col-sm-3 hidden-xs"><img class="cartOrderItemImg" src="{{$i->getImageUrl()}}" alt="{{$i->name}}" style="height: auto; width: 120px;"
-                                        /></div>
-                                    <div class="col-sm-9">
-                                        <h4 class="nomargin cartOrderItemText">{{$i->name}}</h4>
+                    <table id="cart" class="table table-hover table-condensed">
+                        <thead>
+                            <tr>
+                                <th style="width:50%">Product</th>
+                                <th style="width:10%">Price</th>
+                                <th style="width:8%">Quantity</th>
+                                <th style="width:15%" class="text-center">Subtotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{--
+                            <h3>Ingredients</h3> --}} @foreach($order->Ingredients as $i)
+                            <tr>
+                                <td data-th="Product">
+                                    <div class="row">
+                                        <div class="col-sm-3 hidden-xs"><img class="cartOrderItemImg" src="{{$i->getImageUrl()}}" alt="{{$i->name}}" style="height: auto; width: 120px;"
+                                            /></div>
+                                        <div class="col-sm-9">
+                                            <h4 class="nomargin cartOrderItemText">{{$i->name}}</h4>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td data-th="Price">₹{{$i->price}}</td>
-                            <td data-th="Quantity" class="text-center">{{$i->pivot->quantity}}</td>
-                            <td data-th="Subtotal" class="text-center">₹{{$i->pivot->price}}</td>
-                        </tr>
-                        @endforeach {{--
-                        <h3>Recipes</h3> --}} @foreach($order->Recipes as $r)
-                        <tr>
-                            <td data-th="Product">
-                                <div class="row">
-                                    <div class="col-sm-3 hidden-xs"><img class="cartOrderItemImg" src="{{$r->getImageUrl()}}" alt="{{$r->name}}" style="height: auto; width: 120px;"
-                                        /></div>
-                                    <div class="col-sm-9">
-                                        <h4 class="nomargin cartOrderItemText">{{$r->name}}</h4>
+                                </td>
+                                <td data-th="Price">₹{{$i->price}}</td>
+                                <td data-th="Quantity" class="text-center">{{$i->pivot->quantity}}</td>
+                                <td data-th="Subtotal" class="text-center">₹{{$i->pivot->price}}</td>
+                            </tr>
+                            @endforeach {{--
+                            <h3>Recipes</h3> --}} @foreach($order->Recipes as $r)
+                            <tr>
+                                <td data-th="Product">
+                                    <div class="row">
+                                        <div class="col-sm-3 hidden-xs"><img class="cartOrderItemImg" src="{{$r->getImageUrl()}}" alt="{{$r->name}}" style="height: auto; width: 120px;"
+                                            /></div>
+                                        <div class="col-sm-9">
+                                            <h4 class="nomargin cartOrderItemText">{{$r->name}}</h4>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td data-th="Price">₹{{$r->price}}</td>
-                            <td data-th="Quantity" class="text-center">{{$r->pivot->servings}}</td>
-                            <td data-th="Subtotal" class="text-center">₹{{$r->pivot->price}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
+                                </td>
+                                <td data-th="Price">₹{{$r->price}}</td>
+                                <td data-th="Quantity" class="text-center">{{$r->pivot->servings}}</td>
+                                <td data-th="Subtotal" class="text-center">₹{{$r->pivot->price}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
 
-                    <tfoot>
-                        <tr>
-                            <td colspan="3" class="hidden-xs"></td>
-                            <td class="hidden-xs text-center"><strong>Total ₹{{$order->total}}</strong></td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
+                        <tfoot>
+                            <tr>
+                                <td colspan="3" class="hidden-xs"></td>
+                                <td class="hidden-xs text-center"><strong>Total ₹{{$order->total}}</strong></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
         @endforeach
     </div>
 </div>
+@endif
 @endsection
-
+ 
 @section('pagespecificscripts')
 <script>
-
-function updatePBar(orderid, step){
+    function updatePBar(orderid, step){
     $("#steps-" + orderid).remove();
     $("#steps-head-" + orderid).append("<div id='steps-"+ orderid + "'" + "></div>");
     switch(step) {
@@ -107,5 +111,5 @@ function updatePBar(orderid, step){
   steps: steps1
 });
 }
-    </script>
+</script>
 @endsection

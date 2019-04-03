453934f1308f9ffab0b82746fe8d19f635718eb1
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-04-03 13:22:42 +0530

    Cart Design

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,27 +1,98 @@
 @extends('layouts.app') 
 @section('content')
 <div class="pt-4">
-  <h3>Cart</h3>
+  <h2>Cart</h2>
   @if($cart==null)
     <p>No items in cart</p>
   @else
-    @foreach($cart->Ingredients as $i)
-      <p>{{$i->name}}</p>
-      <p>{{$i->pivot->price}}</p>
-      <p>{{$i->pivot->quantity}}</p><!--TBD: editable, when clicked away submit a post form, similar to addToCart-->
-    @endforeach
-    @foreach($cart->Recipes as $r)
-      <p>{{$r->name}}</p>
-      <p>{{$r->pivot->price}}</p>
-      <p>{{$r->pivot->servings}}</p><!--TBD: editable, when clicked away submit a post form, similar to addToCart-->
-      @foreach ($r->Ingredients as $ri)
-        @if($ri->pivot->value>0)
-          <p>{{$ri->name}} {{$ri->pivot->scaledQuantity($r->pivot->servings)}} {{$ri->pivot->Unit->unit_short}}</p>
-        @endif
+  <table id="cart" class="table table-hover table-condensed">
+      <thead>
+      <tr>
+        <th style="width:50%">Product</th>
+        <th style="width:10%">Price</th>
+        <th style="width:8%">Quantity</th>
+        <th style="width:15%" class="text-center">Subtotal</th>
+        <th style="width:17%">Empty Cart <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>		</th>
+      </tr>
+    </thead>
+    <tbody>
+      {{-- <h3>Ingredients</h3> --}}
+      @foreach($cart->Ingredients as $i)
+      <tr>
+        <td data-th="Product">
+          <div class="row">
+            <div class="col-sm-3 hidden-xs"><img src="{{$i->getImageUrl()}}" alt="{{$i->name}}" style="height: auto; width: 120px;"/></div>
+            <div class="col-sm-9">
+              <h4 class="nomargin">{{$i->name}}</h4>
+            </div>
+          </div>
+        </td>
+        <td data-th="Price">₹{{$i->price}}</td>
+        <td data-th="Quantity">
+          <input type="number" class="form-control text-center" value="{{$i->pivot->quantity}}" min="1" max="10" step="1"><!--TBD: editable, when clicked away submit a post form, similar to addToCart-->
+        </td>
+        <td data-th="Subtotal" class="text-center">₹{{$i->pivot->price}}</td>
+        <td class="actions" data-th="">
+            <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+            <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+        </td>
+      </tr>
       @endforeach
-    @endforeach
-    <p>{{$cart->total}}</p>
-  @endif
+
+      {{-- <h3>Recipes</h3> --}}
+      @foreach($cart->Recipes as $r)
+      <tr>
+          <td data-th="Product">
+            <div class="row">
+              <div class="col-sm-3 hidden-xs"><img src="{{$r->getImageUrl()}}" alt="{{$r->name}}" style="height: auto; width: 120px;"/></div>
+              <div class="col-sm-9">
+                <h4 class="nomargin">{{$r->name}}</h4>
+                <p>
+                @foreach ($r->Ingredients as $ri)
+                  @if($ri->pivot->value>0)
+                    {{$ri->name}} {{$ri->pivot->scaledQuantity($r->pivot->servings)}} {{$ri->pivot->Unit->unit_short}}
+                  @endif
+                @endforeach
+                </p>
+              </div>
+            </div>
+          </td>
+          <td data-th="Price">₹{{$r->price}}</td>
+          <td data-th="Quantity">
+            <input type="number" class="form-control text-center" value="{{$r->pivot->servings}}" min="1" max="10" step="1"><!--TBD: editable, when clicked away submit a post form, similar to addToCart-->
+          </td>
+          <td data-th="Subtotal" class="text-center">₹{{$r->pivot->price}}</td>
+          <td class="actions" data-th="">
+              <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+              <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+          </td>
+      </tr>
+      @endforeach
+    </tbody>
+
+    <tfoot>
+      <tr>
+        <td>
+        <h4 style="display: inline;">Address:   </h4>
+          <select style="width: 50%;">
+              <option value="ad1">ad1</option>
+              <option value="ad2">ad2</option>
+              <option value="ad3">ad3</option>
+              <option value="ad4">ad4</option>
+          </select>
+        </td>
+      </tr>
+    
+      <tr>
+          <td><a href="#" class="btn btn-warning">Continue Shopping</a></td>
+          <td colspan="2" class="hidden-xs"></td>
+          <td class="hidden-xs text-center"><strong>Total ₹{{$cart->total}}</strong></td>
+          <td><a href="" class="btn btn-success btn-block">Checkout</a></td>
+        </tr>
+      </tfoot>
+    </table>
+    @endif
+
   <!--TBD: buttons for delete item, empty cart, checkout-->
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
       crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
       crossorigin="anonymous"></script>

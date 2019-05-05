f2107ef7789902686f5235e9985e297b8c8563fe
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-05-06 01:22:27 +0530

    Home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,52 +1,17 @@
 @extends('layouts.app') 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center py-4">
-    <div class="col-md-8">
-      <img style="width: inherit;" src="{{asset('/img/howItWorks.png')}}" />
-    </div>
-  </div>
+<div class="container-fluid">
+
 
   <!--TODO: Same for $recent, $random, $activity-->
 
 
-  <div class="row mt-3 pt-2">
-    <h4>Some recently added dishes...</h4>
-    <div class="offset-md-3 col-md-6">
-      <div id="randomCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          @foreach ($random as $r)
-          <li data-target="#randomCarouselIndicators" data-slide-to="{{ $loop->index }}" class="{{ $loop->first ? 'active' : '' }}"></li>
-          @endforeach
-        </ol>
-        <div class="carousel-inner" role="listbox">
-          @foreach ($random as $r)
-          <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-            <img class="d-block img-fluid" src="{{$r->getImageUrl()}}" alt="{{$r->name}}">
-            <div class="carousel-caption d-none d-md-block" style="background: rgba(0,0,0,0.5); border-radius: 20px; ">
-              <h5 style="color: white; ">{{$r->name}}</h5>
-            </div>
-          </div>
-          @endforeach
-        </div>
-        <a class="carousel-control-prev" href="#randomCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#randomCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
+  
+  <div class="row mt-5">
     <div class="col-md-12">
-      <div class="swiper-container">
+      <div class="swiper-container swiper-recent">
         <div class="swiper-wrapper">
-            @foreach ($random as $r)
-            <a href="{{ url( 'recipes/' . $r->id ) }}">
+            @foreach ($recent as $r)
             <div class="swiper-slide">
                 <img data-src="{{$r->getImageUrl()}}" class="d-block img-fluid swiper-lazy">
                 <div class="carousel-caption d-none d-md-block" style="background: rgba(0,0,0,0.5); border-radius: 20px; ">
@@ -57,12 +22,40 @@
             @endforeach
         </div>
         <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
+        <div class="swiper-pagination swiper-pagination-recent"></div>
+        <div class="swiper-button-prev swiper-button-prev-recent"></div>
+        <div class="swiper-button-next swiper-button-next-recent"></div>
       </div>
     </div>
   </div>
+
+  <div class="row justify-content-center py-4 mt-2">
+      <div class="col-md-6">
+        <img class="img-fluid d-block" style="position:inherit; z-index: 999; width: inherit;" src="{{asset('/img/howItWorks.png')}}" />
+      </div>
+    </div>
+<h4 style="z-index: 999; position: relative; padding-top: 40px">Choose from our large selection...</h4>
+    <div class="row" style="margin-top: -200px">
+        <div class="col-md-12">
+          <div class="swiper-container swiper-random">
+            <div class="swiper-wrapper">
+                @foreach ($random as $r)
+                <div class="swiper-slide">
+                    <img data-src="{{$r->getImageUrl()}}" class="d-block img-fluid swiper-lazy">
+                    <div class="carousel-caption d-none d-md-block" style="background: rgba(0,0,0,0.5); border-radius: 20px; ">
+                        <a href="{{ url( 'recipes/' . $r->id ) }}"><h5 style="color: white; ">{{$r->name}}</h5></a>
+                      </div>
+                    <div class="swiper-lazy-preloader"></div>
+                </div>
+                @endforeach
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination swiper-pagination-random"></div>
+            <div class="swiper-button-prev swiper-button-prev-random"></div>
+            <div class="swiper-button-next swiper-button-next-random"></div>
+          </div>
+        </div>
+      </div>
 
 
 </div>
@@ -70,8 +63,6 @@
 @section('pagespecificscripts')
 <script>
 $(document).ready(function () {
-    
-// Or use this to Open link in same window (similar to target=_blank)
 $(".swiper-slide").click(function(){
     window.location = $(this).find("a:first").attr("href");
     return false;
@@ -79,19 +70,36 @@ $(".swiper-slide").click(function(){
 
 });
 
-  var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 2,
+  var swiper = new Swiper('.swiper-recent', {
+      slidesPerView: 3,
       spaceBetween: 30,
       preloadImages: false,
       loop: true,
       lazy: true,
       watchSlidesVisibility: true,
       navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next-recent',
+      prevEl: '.swiper-button-prev-recent',
       },
       pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper-pagination-recent',
+        clickable: true,
+      },
+    });
+
+    var swiper = new Swiper('.swiper-random', {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      preloadImages: false,
+      loop: true,
+      lazy: true,
+      watchSlidesVisibility: true,
+      navigation: {
+      nextEl: '.swiper-button-next-random',
+      prevEl: '.swiper-button-prev-random',
+      },
+      pagination: {
+        el: '.swiper-pagination-random',
         clickable: true,
       },
     });

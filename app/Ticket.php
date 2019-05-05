49f8a2e9538c666a3e858d1596c7c4b676f2f7d2
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2019-05-05 13:08:50 +0530

    Ticket read status

--- app/Ticket.php
+++ app/Ticket.php
@@ -3,11 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Ticket extends Model
 {
     public function Messages()
     {
         return $this->hasMany('App\TicketMessage', 'ticket_id', 'id');
+    }
+
+    public function getReadStatusAttribute()
+    {
+        if(Auth::User()->hasRole('admin')){
+            return $this->admin_read;
+        }
+        else
+        {
+            return $this->user_read;
+        }
     }
 }

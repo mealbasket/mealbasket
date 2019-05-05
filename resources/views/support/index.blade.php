822de534e46928695cae7e09f0ead3b1dd6e9d77
Craig Pinto <28139867+cp1797@users.noreply.github.com>
2019-05-05 14:01:19 +0530

    Support tickets CSS

--- resources/views/support/index.blade.php
+++ resources/views/support/index.blade.php
@@ -1,11 +1,68 @@
 @extends('layouts.app') 
 @section('content')
-<div class="row">
-    <h2>My Support Tickets</h2>
-    <a class="btn btn-primary" href="/home/support/create" role="button">New ticket</a>
-    @foreach ($tickets as $ticket)
-        <a href="/home/support/{{$ticket->id}}">Ticket {{$ticket->id}}</a>
-        <!--TODO: bold if ticket->unread==1-->
-    @endforeach
+<div class="container">
+    <div class="row">
+        <h2>My Support Tickets</h2>
+        <a class="btn mx-2 px-2 py-2 btn-primary" data-toggle="collapse" href="#newticket" role="button">New Ticket</a>
+    </div>
+    {{-- Please edit form below --}}
+    <form action="/home/support/create" class="pt-4 collapse" id="newticket" method="POST">
+        @csrf {{ method_field('PUT') }}
+        <div class="form-group">
+            <div class="row">
+                <label for="message" class="col-md-4 col-form-label text-md-right">Subject</label>
+                <div class="col-md-6">
+                    <input id="subject" type="text" class="form-control" name="subject" required>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <label for="message" class="col-md-4 col-form-label text-md-right">Message</label>
+                <div class="col-md-6">
+                    <input id="message" type="text" class="form-control" name="message" required>
+                </div>
+            </div>
+        </div>
+        <div class="my-4 form-group row mb-0">
+            <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+<div class="row mt-4">
+    <table class="table table-bordered text-center">
+        <thead class="thead-dark">
+            <tr>
+                <th>Ticket ID</th>
+                <th>Subject</th>
+                <th>Status</th>
+                <th>Creation Date</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tickets as $ticket) @if ($ticket->read_status==0)
+            <tr class="table-warning" style="font-weight: bold">
+                <td>{{$ticket->id}}</td>
+                <td>{{$ticket->subject}}</td>
+                <td>{{$ticket->status ? 'Solved': 'Pending - New Message'}}</td>
+                <td>{{$ticket->created_at->diffForHumans()}}</td>
+                <td>
+                    <a class="btn btn-primary" href="/home/support/{{$ticket->id}}" role="button">View</a>
+                </td>
+            </tr>
+            @else
+            <tr>
+                <td>{{$ticket->id}}</td>
+                <td>{{$ticket->subject}}</td>
+                <td>{{$ticket->status ? 'Solved': 'Pending - No New Messages'}}</td>
+                <td>{{$ticket->created_at->diffForHumans()}}</td>
+                <td>
+                    <a class="btn btn-primary" href="/home/support/{{$ticket->id}}" role="button">View</a>
+                </td>
+            </tr>
+            @endif @endforeach
+        </tbody>
+    </table>
+</div>
 </div>
 @endsection
--- resources/views/support/show.blade.php
+++ resources/views/support/show.blade.php
@@ -1,34 +1,40 @@
 @extends('layouts.app') 
 @section('content')
 <div class="container">
-    <h2>Ticket ID: {{$ticket->id}} Status: {{$ticket->status ? 'Solved': 'Pending'}}</h2>
-    <a class="btn btn-primary" data-toggle="collapse" href="#reply" role="button">Add reply</a>
+  <h4>Ticket Subject: {{$ticket->subject}} - {{$ticket->id}}</h4>
+  <h5>Status: {{$ticket->status ? 'Solved': 'Pending'}}</h4>
+    <a class="btn btn-primary" data-toggle="collapse" href="#reply" role="button">Add Message/Reply</a>
     <form action="/home/support/{{$ticket->id}}" class="pt-4 collapse" id="reply" method="POST">
-      @csrf
-      {{ method_field('PUT') }}
+      @csrf {{ method_field('PUT') }}
       <div class="form-group row">
-          <label for="message" class="col-md-4 col-form-label text-md-right">Message</label>
-              <div class="col-md-6">
-                  <input id="message" type="text" class="form-control" name="message" required>
-              </div>
+        <label for="message" class="col-md-4 col-form-label text-md-right">Message</label>
+        <div class="col-md-6">
+          <input id="message" type="text" class="form-control" name="message" required>
+        </div>
       </div>
-      <div class="form-group row mb-0">
-          <div class="col-md-6 offset-md-4">
-              <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
+      <div class="my-4 form-group row mb-0">
+        <div class="col-md-6 offset-md-4">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
       </div>
-  </form>
-  @if($ticket->status==0)
+    </form>
+    @if($ticket->status==0)
     <button class="btn btn-primary" onclick="event.preventDefault();document.getElementById('status-form').submit();">Mark Solved</button>
     <form id="status-form" action="/home/support/{{$ticket->id}}" method="POST" style="display: none;">
       @csrf
     </form>
-  @endif
-  @foreach($messages as $message)
-    <p>msg:{{$message->message}}
-      date:{{$message->created_at->diffForHumans()}}
-      by:{{$message->User->name}}
-    </p>
-  @endforeach
+
+    @endif @foreach($messages as $message)
+    <div class="row mt-3">
+      <div class="col-md-12">
+        <blockquote class="supportMessage blockquote">
+          <p class="mb-0">{{$message->message}}</p>
+          <footer class="blockquote-footer">
+            Created: {{$message->created_at->diffForHumans()}} by {{$message->User->name}}
+          </footer>
+        </blockquote>
+      </div>
+    </div>
+    @endforeach
 </div>
 @endsection
